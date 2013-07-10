23889c3b309d8be43a030e54f667af6bc35a1cd8
doun <doun@163.com>
2013-07-10 12:23:39 +0800

    增加刚才漏掉的文件

--- parser/parser.go
+++ parser/parser.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"bufio"
+	"code.google.com/p/log4go"
 	"errors"
+	"fmt"
 	"io/ioutil"
+	"os"
 	str "strings"
 	"time"
 )
 
+var log log4go.Logger
+
+func init() {
+	log = make(log4go.Logger)
+	log.AddFilter("stdout", log4go.INFO, log4go.NewFormatLogWriter(os.Stdout, "[%D %T][%L][Conn](%S) %M"))
+}
+
 type GammaRPT struct {
 	SVol         float32
+	SVolUnit     string
 	STitle       string
 	SType        string
 	AcqStartTime time.Time
 	LiveSeconds  int32
 	RptGenTime   time.Time
+	EngCalTime   time.Time
+	EffCalTime   time.Time
 	Nuclides     map[string]NuclideActivity
 	file         *string
 	fReader      *str.Reader
@@ -36,14 +49,32 @@ func (self *GammaRPT) Parse(filePath string) error {
 	self.file = &file
 	self.fReader = str.NewReader(file)
 	self.bufReader = bufio.NewReader(self.fReader)
+
 	volStart := str.Index(file, "Sample Size")
 	if volStart < 0 {
 		return errors.New("未找到样品体积")
 	}
 	self.fReader.Seek(int64(volStart), 0)
-	vol,err := self.bufReader.ReadLine()
-	if err != nil{
+	volStr, _, err := self.bufReader.ReadLine()
+	if err != nil {
+		log.Debug("未读到体积行,已经找到的为:", volStr)
 		panic(err)
 	}
+	volParts := str.Split(string(volStr), ":")
+	if len(volParts) != 2 {
+		panic("体积解析出错")
+	}
+	volVals := str.Split(str.TrimSpace(volParts[1]), " ")
+	volValStr := volVals[0]
+	volUnit := volVals[1]
+
+	fmt.Sscanf(volValStr, "%f", &self.SVol)
+	fmt.Sscanf(volUnit, "%s", &self.SVolUnit)
+
+	titleStart := str.Index(file, "Sample Title")
+	if titleStart < 0 {
+		return errors.New("未找到样品名称")
+	}
+	self.fReader.Seek(int64(titleStart), 0)
 	return nil
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"testing" 
-	"strings"
 	"bufio"
 	"fmt"
+	"strings"
+	"testing"
 )
 
 func TestSeekString(t *testing.T) {
@@ -13,10 +13,32 @@ func TestSeekString(t *testing.T) {
 	asdf11
 	dd`
 	r := strings.NewReader(s)
-	r.Seek(1,0)
+	r.Seek(1, 0)
 	buf := bufio.NewReader(r)
-	bb,_,_ := buf.ReadLine()
+	bb, _, _ := buf.ReadLine()
 	fmt.Println(string(bb))
 
 }
 
+func TestFmtScanf(t *testing.T) {
+	s := "asdf 123"
+	var s1 string
+	var i1 int32
+	_, e := fmt.Sscanf(s, "%*s %d",&s1, &i1)
+	if e!= nil{
+		fmt.Println(e)
+		t.Fail()
+	}
+	if i1 != 123 {
+		t.Fail()
+	}
+}
+
+func TestCanDecodeVol(t *testing.T) {
+	fpath := "./testfiles/1升液体.RPT"
+	parser := new(GammaRPT)
+	parser.Parse(fpath)
+	if parser.SVol != 0.001 {
+		t.Fail()
+	}
+}
